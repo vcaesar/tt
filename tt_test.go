c51cbddb0d963279f87ae81c8f1f16e1cff2ec5d
vcaesar <vzvway@gmail.com>
2018-05-25 23:41:09 +0800

    add more test

--- tt_test.go
+++ tt_test.go
@@ -8,6 +8,23 @@ import (
 )
 
 func TestAdd(t *testing.T) {
+	mockT := new(testing.T)
+
+	fmt.Println(Expect(mockT, "1", add.Add(1, 1)))
+	if Expect(mockT, "1", add.Add(1, 1)) {
+		t.Error("Equal should return false")
+	}
+	if Equal(mockT, "1", add.Add(1, 1)) {
+		t.Error("Equal should return false")
+	}
+
+	if !Expect(mockT, "Hello World", "Hello World") {
+		t.Error("Equal should return true")
+	}
+	if !Equal(mockT, "Hello World", "Hello World") {
+		t.Error("Equal should return true")
+	}
+
 	fmt.Println(add.Add(1, 1))
 
 	// Expect(t, "1", add.Add(1, 1))
